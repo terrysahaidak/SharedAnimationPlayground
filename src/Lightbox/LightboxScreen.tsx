3ac33fb28f87b2123c23a9b250be8abdf327b3e7
Terry Sahaidak <tasssik@gmail.com>
2023-03-30 21:09:01 +0300

    Use new API

--- src/Lightbox/LightboxScreen.tsx
+++ src/Lightbox/LightboxScreen.tsx
@@ -1,36 +1,31 @@
 import React from 'react';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import Animated, {
-  Easing,
   Extrapolate,
   FadeIn,
   FadeOut,
   interpolate,
   runOnJS,
   SharedTransition,
-  useAnimatedGestureHandler,
   useAnimatedStyle,
   useSharedValue,
+  withSpring,
   withTiming,
 } from 'react-native-reanimated';
 import {Pressable, StyleSheet, View} from 'react-native';
-import {
-  PanGestureHandler,
-  PanGestureHandlerGestureEvent,
-} from 'react-native-gesture-handler';
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
 
-const timingConfig = {
-  duration: 240,
-  easing: Easing.bezierFn(0.33, 0.01, 0, 1),
+const springOptions = {
+  damping: 15,
 };
 
 export const lightboxTransition = SharedTransition.custom(values => {
   'worklet';
   return {
-    width: withTiming(values.targetWidth, timingConfig),
-    height: withTiming(values.targetHeight, timingConfig),
-    originX: withTiming(values.targetOriginX, timingConfig),
-    originY: withTiming(values.targetOriginY, timingConfig),
+    width: withSpring(values.targetWidth, springOptions),
+    height: withSpring(values.targetHeight, springOptions),
+    originX: withSpring(values.targetOriginX, springOptions),
+    originY: withSpring(values.targetOriginY, springOptions),
   };
 });
 
@@ -50,66 +45,67 @@ export function LightboxScreen() {
     scale: useSharedValue(1),
   };
 
-  type AnimatedGHContext = {
-    startX: number;
-    startY: number;
-  };
-  const gestureHandler = useAnimatedGestureHandler<
-    PanGestureHandlerGestureEvent,
-    AnimatedGHContext
-  >({
-    onStart: (_, ctx) => {
-      ctx.startX = translation.x.value;
-      ctx.startY = translation.y.value;
-    },
-    onActive: (event, ctx) => {
-      translation.x.value = ctx.startX + event.translationX;
-      translation.y.value = ctx.startY + event.translationY;
-      translation.scale.value = interpolate(
+  const opacity = useSharedValue(1);
+
+  const panGeture = Gesture.Pan()
+    .onChange(event => {
+      translation.x.value += event.changeX;
+      translation.y.value += event.changeY;
+      opacity.value = interpolate(
         translation.y.value,
         [-200, 0, 200],
-        [0.65, 1, 0.65],
+        [0, 1, 0],
         Extrapolate.CLAMP,
       );
-    },
-    onEnd: _ => {
+    })
+    .onEnd(() => {
       if (Math.abs(translation.x.value) + Math.abs(translation.y.value) > 150) {
         runOnJS(goBack)();
       } else {
         translation.x.value = withTiming(0);
         translation.y.value = withTiming(0);
-        translation.scale.value = withTiming(1);
       }
-    },
-  });
+    });
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [
       {translateX: translation.x.value},
       {translateY: translation.y.value},
-      {scale: translation.scale.value},
+      {
+        scale:
+          1 -
+          (Math.abs(translation.x.value) + Math.abs(translation.y.value)) /
+            1000,
+      },
     ],
-    width: activeItem.width,
-    height: activeItem.height,
   }));
 
   return (
     <View style={[styles.container]}>
       <Animated.View
         entering={FadeIn.duration(200)}
-        exiting={FadeOut.duration(200).delay(0)}
-        style={StyleSheet.absoluteFill}>
+        style={[
+          StyleSheet.absoluteFill,
+          {
+            opacity,
+          },
+        ]}>
         <Pressable style={styles.backdrop} onPress={goBack} />
       </Animated.View>
 
-      <PanGestureHandler onGestureEvent={gestureHandler}>
-        <Animated.Image
-          sharedTransitionStyle={lightboxTransition}
-          source={{uri: activeItem.uri}}
-          style={[animatedStyle]}
-          sharedTransitionTag={'image-' + activeItem.id}
-        />
-      </PanGestureHandler>
+      <GestureDetector gesture={panGeture}>
+        <Animated.View style={[{flex: 1}, animatedStyle]}>
+          <Animated.Image
+            sharedTransitionStyle={lightboxTransition}
+            source={{uri: activeItem.uri}}
+            style={{
+              width: activeItem.width,
+              height: activeItem.height,
+            }}
+            sharedTransitionTag={'image-' + activeItem.id}
+          />
+        </Animated.View>
+      </GestureDetector>
 
       <Animated.Text
         entering={FadeIn}
